8f8145999483f86c704de8e0d1189b5ecce2548a
Luis Dourado <lfaddourado@gmail.com>
2024-08-04 18:30:01 -0300

    feat(application): listen for siginterrupt signal for graceful shutdown

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/signal"
 
 	"github.com/Lutefd/gorders-api/application"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load(".env")
 	app := application.NewApp()
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	err := app.Start(ctx)
 	if err != nil {
 		panic(err)
 	}

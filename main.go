21836ddb13e53f7a0e085d638d45f2362940dc91
Luis Dourado <lfaddourado@gmail.com>
2024-08-04 17:58:56 -0300

    feat(middleware): add logger

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	router := chi.NewRouter()
+	router.Use(middleware.Logger)
 	router.Get("/hello", hello)
 	server := &http.Server{
 		Addr:    ":8080",

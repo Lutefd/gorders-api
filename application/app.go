61b97a9fbc941a491788677ed6e3fe738de92228
Luis Dourado <lfaddourado@gmail.com>
2024-08-04 18:29:40 -0300

    feat(application): add redis connection and graceful shutdown

--- application/app.go
+++ application/app.go
@@ -4,15 +4,26 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func NewApp() *App {
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: redisPassword,
+	})
 	return &App{
 		router: loadRoutes(),
+		rdb:    rdb,
 	}
 }
 
@@ -21,9 +32,32 @@ func (a *App) Start(ctx context.Context) error {
 		Addr:    ":8080",
 		Handler: a.router,
 	}
-	err := server.ListenAndServe()
+	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+		return fmt.Errorf("failed to connect to redis: %w", err)
 	}
-	return nil
+	defer func() {
+		err := a.rdb.Close()
+		if err != nil {
+			fmt.Println("failed to close redis connection")
+		}
+	}()
+	fmt.Println("Starting server on port 8080")
+	ch := make(chan error, 1)
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		_, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		return server.Shutdown(ctx)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/Lutefd/gorders-api
 
 go 1.22.3
 
-require github.com/go-chi/chi/v5 v5.1.0 // indirect
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/redis/go-redis/v9 v9.6.1 // indirect
+)
